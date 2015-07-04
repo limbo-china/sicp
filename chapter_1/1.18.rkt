b840ad240b77cf9b5dcbe6ee40c31e4218c55658
limbo-china <362019841@qq.com>
2015-07-05 01:19:14 +0800

    commit
    commit

--- chapter_1/1.18.rkt
+++ chapter_1/1.18.rkt
@@ -3,3 +3,17 @@
 (define (double x)(+ x x))
 (define (havle x )(/ x 2))
 
+(define (even? x)
+  (= (remainder x 2) 0))
+
+(define (multi a b)
+  (mul a b 0))
+
+(define (mul a b product)
+  (cond((= b 0) product)
+       ((even? b)(mul (double a) (havle b) product))
+       (else (mul a (- b 1) (+ product a)))))
+(trace mul)
+(multi 2 3)
+(multi 1 6)
+(multi 7 8)
